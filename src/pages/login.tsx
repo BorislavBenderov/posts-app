9f68688f5c9ba251d77c165ac9a749353cca3d59
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-17 09:34:17 +0200

    Add possibility users to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,22 @@
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+  const navigate = useNavigate();
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
-    <div>Login page</div>
-  )
-}
+    <div>
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    </div>
+  );
+};
