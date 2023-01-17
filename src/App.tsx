8e98e4ad245f93accdd7fd2cfbe3f606fee0e0ea
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-17 12:09:24 +0200

    Change path of main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { CreatePost } from "./pages/create-post/create-post";
 import { Login } from "./pages/login";
-import { Main } from "./pages/main";
+import { Main } from "./pages/main/main";
 
 function App() {
   return (
